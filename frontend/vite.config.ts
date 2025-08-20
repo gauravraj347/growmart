0a47b541012924651040a643c181a5e17409f534
gauravraj347 <127657698+gauravraj347@users.noreply.github.com>
2025-08-20 15:13:21 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true
-  },
-  server: {
-    port: 3000,
-    strictPort: true
-  },
-  preview: {
-    port: 3000,
-    strictPort: true
-  }
-})
+});
